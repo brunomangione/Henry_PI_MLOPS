ef9504e255f6e5b00e0589a56fec1dc7ee3e017a
Bruno <brunomangione@gmail.com>
2023-11-03 10:36:48 -0300

    cambios

--- recommender.py
+++ recommender.py
@@ -1,7 +1,9 @@
+
 import pandas as pd
 import numpy as np
-from sklearn.decomposition import PCA
+from sklearn.decomposition import TruncatedSVD
 from sklearn.preprocessing import normalize
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def load_recommendation_data(data_file):
     recommend = pd.read_parquet(data_file)
@@ -14,15 +16,14 @@ def initialize_recommendation_system(recommend):
     # Crear un diccionario para mapear índices a nombres de juegos
     item_to_idx = {item: idx for idx, item in enumerate(unique_items)}
 
-    # Crear una matriz TF-IDF manualmente
-    from sklearn.feature_extraction.text import TfidfVectorizer
+    # Crear una matriz TF-IDF
     tfidf_vectorizer = TfidfVectorizer()
     tfidf_matrix = tfidf_vectorizer.fit_transform(corpus)
 
-    # Reducción de dimensionalidad con PCA
+    # Reducción de dimensionalidad con TruncatedSVD
     n_components = 100  # Ajusta este valor según tus necesidades
-    pca = PCA(n_components=n_components)
-    tfidf_matrix = pca.fit_transform(tfidf_matrix)
+    svd = TruncatedSVD(n_components=n_components)
+    tfidf_matrix = svd.fit_transform(tfidf_matrix)
     tfidf_matrix = normalize(tfidf_matrix)  # Normalizar la matriz
 
     # Calcular la similitud de coseno
